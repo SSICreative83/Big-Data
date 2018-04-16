fc892fa81c8e9aa7c5ec17cd6e58b505a16c020b
Pierre Navaro <pierre.navaro@univ-rennes1.fr>
2018-04-16 16:27:26 +0200

    move notebooks

--- tests.py
+++ tests.py
@@ -2,13 +2,22 @@ import os
 import subprocess
 import tempfile
 import nbformat
+import sys
+from pathlib import Path
+import nbconvert
+from nbconvert.preprocessors import ExecutePreprocessor
+import glob
 
-os.chdir('2017')
+print("Running Notebooks...")
+
+os.chdir('notebooks')
 
 def _notebook_run(nb_file):
+
     """Execute a notebook via nbconvert and collect output.
        :returns (parsed nb object, execution errors)
     """
+
     with tempfile.NamedTemporaryFile(suffix=".ipynb") as fout:
         args = ["jupyter", "nbconvert", "--to", "notebook", "--execute",
           "--ExecutePreprocessor.timeout=500",
@@ -23,67 +32,67 @@ def _notebook_run(nb_file):
                      if output.output_type == "error"]
     return nb, errors
 
-def test_ipynb_2017_01():
+def test_ipynb_01():
     here = os.getcwd()
     nb, errors = _notebook_run('01.MapReduce.ipynb')
     assert errors == []
 
-def test_ipynb_2017_02():
+def test_ipynb_02():
     here = os.getcwd()
     nb, errors = _notebook_run('02.Containers.ipynb')
     assert errors == []
 
-def test_ipynb_2017_03():
+def test_ipynb_03():
     here = os.getcwd()
     nb, errors = _notebook_run('03.ParallelComputation.ipynb')
     assert errors == []
 
-def test_ipynb_2017_04():
+def test_ipynb_04():
     here = os.getcwd()
     nb, errors = _notebook_run('04.AsynchronousProcessing.ipynb')
     assert errors == []
     
-def test_ipynb_2017_05():
+def test_ipynb_05():
     here = os.getcwd()
     nb, errors = _notebook_run('05.UnixCommands.ipynb')
     assert errors == []
 
-#def test_ipynb_2017_06():
+#def test_ipynb_06():
 #    here = os.getcwd()
 #    nb, errors = _notebook_run('06.Hadoop.ipynb')
 #    assert errors == []
 #
-#def test_ipynb_2017_07():
+#def test_ipynb_07():
 #    here = os.getcwd()
 #    nb, errors = _notebook_run('07.PySpark.ipynb')
 #    assert errors == []
 #
-#def test_ipynb_2017_08():
+#def test_ipynb_08():
 #    here = os.getcwd()
 #    nb, errors = _notebook_run('08.Dask.ipynb')
 #    assert errors == []
 #
-#def test_ipynb_2017_09():
+#def test_ipynb_09():
 #    here = os.getcwd()
 #    nb, errors = _notebook_run('09.PandasSeries.ipynb')
 #    assert errors == []
 #
-#def test_ipynb_2017_10():
+#def test_ipynb_10():
 #    here = os.getcwd()
 #    nb, errors = _notebook_run('10.DaskDataframes.ipynb')
 #    assert errors == []
 #
-#def test_ipynb_2017_11():
+#def test_ipynb_11():
 #    here = os.getcwd()
 #    nb, errors = _notebook_run('11.PySparkHadoop.ipynb')
 #    assert errors == []
 #
-#def test_ipynb_2017_12():
+#def test_ipynb_12():
 #    here = os.getcwd()
 #    nb, errors = _notebook_run('12.NYCTaxiCabTrip.ipynb')
 #    assert errors == []
 #
-#def test_ipynb_2017_13():
+#def test_ipynb_13():
 #    here = os.getcwd()
 #    nb, errors = _notebook_run('13.ExamCorrection.ipynb')
 #    assert errors == []
